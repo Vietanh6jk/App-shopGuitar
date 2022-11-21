1f6b2e663f9837b609850c6bef1f65e7d6fff91a
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-21 16:43:28 +0700

    finish flatlist1 screen 3(50%)

--- src/components/ComponenHome.js
+++ src/components/ComponenHome.js
@@ -1,15 +1,131 @@
-import { View, Text, Button, StatusBar, TextInput, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Button, StatusBar, TextInput, Image, TouchableOpacity, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import IconFontAwesome from 'react-native-vector-icons/FontAwesome'
 import IconIonicons from 'react-native-vector-icons/Ionicons'
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-import styles from '../themes/styles'
-import color from '../themes/color'
+import styles from '../themes/styles';
+import color from '../themes/color';
+import data from '../database/data';
 
 const ComponenHome = ({ navigation }) => {
 
-    
+    //
+    const [dbGuitar, setDbGuitar] = useState([]);
+
+    //
+    let { z_item, listItem } = data;
+
+    //
+    useEffect(() => {
+        setDbGuitar(listItem)
+    }, [])
+
+    //
+    const renderGuitar = ({ item }) => {
+
+        let str = '';
+        let a = item.price.toString();
+        let x = 0;
+        if (a.length > 3) {
+            let i = 0;
+            for (var key of a) {
+                if ((a.length - i < 2)) {
+                    str += key + "";
+                    continue
+                }
+
+                switch (a.length % 3) {
+                    case 1:
+                        x++;
+                        if (x == 1) {
+                            str += key + ",";
+                        }
+                        else {
+                            str += key + "";
+                        }
+
+                        if(x == 3)
+                        {
+                            x = 0;
+                        }
+                        break;
+                    case 2:
+                        x++;
+                        if (x == 2) {
+                            str += key + ",";
+                        }
+                        else {
+                            str += key + "";
+                        }
+
+                        if(x == 3)
+                        {
+                            x = 0;
+                        }
+                        break;
+                    case 0:
+                        x++;
+                        if (x == 3) {
+                            str += key + ",";
+                        }
+                        else {
+                            str += key + "";
+                        }
+
+                        if(x == 3)
+                        {
+                            x = 0;
+                        }
+                        break;
+
+                    default:
+                        str += key;
+                        break;
+                }
+                i++ ;
+            }
+        }
+
+        let str_price = str;
+
+
+        return (
+            <View style={[, {
+                width: 160,
+                minHeight: 200,
+                backgroundColor: '#fff',
+                borderRadius: 10,
+                paddingTop: 20,
+                alignItems: 'center',
+                marginRight: 15,
+            }]} >
+                <Image style={[, { width: 150, height: 150, }]} source={{ uri: item.img }} />
+                <View style={[, { padding: 10, }]}  >
+                    <Text style={[, {
+                        fontSize: 12,
+                        fontWeight: 'bold',
+                    }]} >{item.name}</Text>
+                    <Text style={[, {
+                        fontSize: 13,
+                        color: color.color_main,
+                        marginTop: 10,
+                    }]} >$ {str_price}</Text>
+                </View>
+
+                <TouchableOpacity style={[, {
+                    position: 'absolute',
+                    top: 10,
+                    right: 10,
+                }]}
+
+                >
+                    <IconMaterialCommunityIcons name={item.favourite ?'heart':'heart-outline' } size={25} color={item.favourite ?'#f00':'#000' } />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View style={[styles.flex_1, {
             backgroundColor: color.color_bg,
@@ -46,8 +162,9 @@ const ComponenHome = ({ navigation }) => {
 
                 </View>
             </View>
-            {/*  */}
 
+
+            {/*  */}
             <View style={[, {
                 // backgroundColor:'#0ff',
                 marginTop: 20,
@@ -75,10 +192,50 @@ const ComponenHome = ({ navigation }) => {
 
             {/* list item */}
 
-            <View>
-                <View>
-                    <Image  style={[  ,{width: 150, height: 150,}]}  source={{uri:'https://guitar.station.vn/wp-content/uploads/2015/11/HD-300-Top-vertical-Copy-2-500x500.jpg'}} />
-                </View>
+            <View style={[styles.row, {
+                marginTop: 30,
+            }]} >
+                {/* item */}
+                {/* <View style={[, {
+                    width: 160,
+                    minHeight: 200,
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+                    paddingTop: 20,
+                    alignItems: 'center',
+                    marginRight:15,
+                }]} >
+                    <Image style={[, { width: 150, height: 150, }]} source={{ uri: z_item.img }} />
+                    <View style={[, { padding: 10 }]}  >
+                        <Text style={[, {
+                            fontSize: 12,
+                            fontWeight: 'bold',
+                        }]} >{z_item.name}</Text>
+                        <Text style={[, {
+                            fontSize: 13,
+                            color: color.color_main,
+                            marginTop: 10,
+                        }]} >$ {z_item.price}</Text>
+                    </View>
+
+                    <TouchableOpacity style={[, {
+                        position: 'absolute',
+                        top: 10,
+                        right: 10,
+                    }]} 
+                    
+                    >
+                        <IconMaterialCommunityIcons name='heart' size={25} color='#f00' />
+                    </TouchableOpacity>
+                </View> */}
+
+                <FlatList
+                    horizontal={true}
+                    data={dbGuitar}
+                    keyExtractor={item => item.id}
+                    renderItem={renderGuitar}
+                />
+
             </View>
 
 
---
+++ src/database/data.js
@@ -0,0 +1,112 @@
+let z_item ={
+    id:1,
+    name:'Guitar Acoustic Station HD-199',
+    price:1990,
+    favourite:true,
+    sale:30,
+    img:'https://guitar.station.vn/wp-content/uploads/2015/11/HD-300-Top-vertical-Copy-2-500x500.jpg',
+}
+
+let listItem = [
+    {
+        id:1,
+        name:'Guitar Acoustic Station HD-199',
+        price:1990,
+        favourite:true,
+        sale:30,
+        img:'https://guitar.station.vn/wp-content/uploads/2015/11/HD-300-Top-vertical-Copy-2-500x500.jpg',
+    },
+    {
+        id:2,
+        name:'Guitar Acoustic EKO D-Blue',
+        price:2990,
+        favourite:true,
+        sale:9,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/03/EKO-D-BLUE-TOP-Copy-360x360.png',
+    },
+    {
+        id:3,
+        name:'Guitar Acoustic EKO ONE 018 CW EQ Sunbust',
+        price:4500,
+        favourite:false,
+        sale:30,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/12/dan-guitar-acoustic-eko-one-series-sunbust-3-500x500.jpg',
+    },
+    {
+        id:4,
+        name:'Guitar Classic Station C-199',
+        price:1990,
+        favourite:false,
+        sale:0,
+        img:'https://guitar.station.vn/wp-content/uploads/2015/11/guitar-classic-cl-150-500x500.jpg',
+    },
+    {
+        id:5,
+        name:'Guitar Classic Station CL450',
+        price:5500,
+        favourite:true,
+        sale:6,
+        img:'https://guitar.station.vn/wp-content/uploads/2016/04/CL-150-TOP-vertical-500x500.jpg',
+    },
+    {
+        id:6,
+        name:'Guitar Acoustic EKO ONE 018 CW EQ Natural',
+        price:4500,
+        favourite:false,
+        sale:30,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/12/dan-guitar-acoustic-eko-one-series-sunbust-3-500x500.jpg',
+    },
+    {
+        id:7,
+        name:'Guitar Acoustic Epiphone DR-100-Natural',
+        price:3190,
+        favourite:true,
+        sale:15,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/01/dan-guitar-acoustic-epiphone-dr100-chinh-hang-1-360x360.jpg',
+    },
+    {
+        id:8,
+        name:'Guitar Classic Admira Malaga',
+        price:7500,
+        favourite:true,
+        sale:5,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/12/dan-guitar-classic-malaga-3-500x500.jpg',
+    },
+    {
+        id:9,
+        name:'Guitar Classic Station CL-350',
+        price:3000,
+        favourite:false,
+        sale:45,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/03/Untitled-1-Copy-2-1-500x500.jpg',
+    },
+    {
+        id:10,
+        name:'Guitar Acoustic Station CC300A',
+        price:6500,
+        favourite:true,
+        sale:12,
+        img:'https://guitar.station.vn/wp-content/uploads/2016/04/CC-300A-Top-vertical-500x500.jpg',
+    },
+    {
+        id:11,
+        name:'Guitar Acoustic EKO-EQ-Black-OM',
+        price:3990,
+        favourite:true,
+        sale:20,
+        img:'https://guitar.station.vn/wp-content/uploads/2018/03/EKO-A-EQ-BLACK-OM-TOP-Copy-500x500.png',
+    },
+    {
+        id:12,
+        name:'Guitar Acoustic Xanh Lá Diduo',
+        price:3200,
+        favourite:true,
+        sale:25,
+        img:'https://guitar.station.vn/wp-content/uploads/2020/05/dan-guitar-xanh-la-1-Copy-500x500.jpg',
+    },
+]
+
+export default  {
+  z_item:z_item,
+  listItem
+}
